674577e502bafe82fa30a1f71211771705601894
gohar <goharusman1999@gmail.com>
2021-12-10 15:04:43 +0500

    version 2.1

--- ToasterMessage/src/main/java/com/example/toastermessage/MyToaster.java
+++ ToasterMessage/src/main/java/com/example/toastermessage/MyToaster.java
@@ -4,9 +4,14 @@ import android.content.Context;
 import android.widget.Toast;
 
 public class MyToaster {
-    public static void s(Context c, String message){
 
-        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
-
+    public static int sum(int a, int b){
+        return a+b;
     }
+
+//    public static void s(Context c, String message){
+//
+//        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+//
+//    }
 }
